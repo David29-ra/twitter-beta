6f71e0b0b727dc14a48673ce9cafd230a954fb72
David29-ra <davidrivasa.12@gmail.com>
2021-08-27 18:59:31 +0000

    Add methods on controllers (tweet, comment), in app controller add the permitted method to add attributes on devise

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,65 @@
 class CommentsController < ApplicationController
+  # GET /comments
+  def index
+    @comments = Comment.all
+  end
+
+  # GET /comments/:id
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  # GET /comments/new
+  def new
+    @comment = Comment.new
+  end
+
+  # GET /comments/:id/edit
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  # POST /comments
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      flash[:success] = 'Object successfully created'
+      redirect_to @comment
+    else
+      flash[:error] = 'Something went wrong'
+      render :new
+    end
+  end
+
+  # PATCH /comments/:id
+  def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      flash[:success] = 'comment was successfully updated'
+      redirect_to @comment
+    else
+      flash[:error] = 'Something went wrong'
+      render :edit
+    end
+  end
+
+  # DELETE /comments/:id
+  def destroy
+    @comment = Comment.find(params[:id])
+
+    if @comment.destroy
+      flash[:success] = 'comment was successfully deleted.'
+    else
+      flash[:error] = 'Something went wrong'
+    end
+    redirect_to comments_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,65 @@
 class TweetsController < ApplicationController
+  # GET /tweets
+  def index
+    @tweets = Tweet.all
+  end
+
+  # GET /tweets/:id
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  # GET /tweets/new
+  def new
+    @tweet = Tweet.new
+  end
+
+  # GET /tweets/:id/edit
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  # POST /tweets
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    if @tweet.save
+      flash[:success] = 'Object successfully created'
+      redirect_to @tweet
+    else
+      flash[:error] = 'Something went wrong'
+      render :new
+    end
+  end
+
+  # PATCH /tweets/:id
+  def update
+    @tweet = Tweet.find(params[:id])
+
+    if @tweet.update(tweet_params)
+      flash[:success] = 'Tweet was successfully updated'
+      redirect_to @tweet
+    else
+      flash[:error] = 'Something went wrong'
+      render :edit
+    end
+  end
+
+  # DELETE /tweets/:id
+  def destroy
+    @tweet = Tweet.find(params[:id])
+
+    if @tweet.destroy
+      flash[:success] = 'Tweet was successfully deleted.'
+    else
+      flash[:error] = 'Something went wrong'
+    end
+    redirect_to tweets_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
 end
