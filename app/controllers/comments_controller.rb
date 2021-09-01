edce759703506978a13e1a47b95d84d0772675bc
David29-ra <davidrivasa.12@gmail.com>
2021-09-01 23:40:26 +0000

    final solution

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,12 +22,12 @@ class CommentsController < ApplicationController
 
   # PATCH /comments/:id
   def update
+    @tweet = Tweet.find(params[:tweet_id])
     @comment = Comment.find(params[:id])
-    @comment.user = current_user
 
-    if @comment.update(comment_params)
+    if @comment.update(comment_update_params)
       flash[:success] = 'comment was successfully updated'
-      redirect_to @comment
+      redirect_to @tweet
     else
       flash[:error] = 'Something went wrong'
       render :edit
@@ -36,19 +36,19 @@ class CommentsController < ApplicationController
 
   # DELETE /comments/:id
   def destroy
+    @tweet = Tweet.find(params[:tweet_id])
     @comment = Comment.find(params[:id])
-
-    if @comment.destroy
-      flash[:success] = 'comment was successfully deleted.'
-    else
-      flash[:error] = 'Something went wrong'
-    end
-    redirect_to comments_path
+    @comment.destroy
+    redirect_to @tweet
   end
 
   private
 
   def comment_params
     params.permit(:body, :tweet_id)
+  end
+
+  def comment_update_params
+    params.require(:comment).permit(:body, :tweet_id)
   end
 end
